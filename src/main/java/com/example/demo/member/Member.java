9c2f44b5c271715e01de6a4cd1a5448e98c03a7c
heejun1105 <163511743+heejun1105@users.noreply.github.com>
2024-09-23 21:20:44 +0900

    member 엔티티추가

---
+++ src/main/java/com/example/demo/member/Member.java
@@ -0,0 +1,40 @@
+package com.example.demo.member;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "MEMBER")
+public class Member {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq")
+    @SequenceGenerator(name = "member_seq", sequenceName = "member_seq", allocationSize = 1)
+	private Long memberNum;
+	
+	private String memberId;
+	
+	private String password;
+	
+	private LocalDateTime createDate;
+	
+	private String email;
+	
+}
--- src/main/java/com/example/member/Member.java
+++
@@ -1,6 +0,0 @@
-package com.example.member;
-
-
-public class Member {
-
-}
