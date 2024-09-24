59f3d84407eb828470b631b791dc2ff48c1f9668
heejun1105 <163511743+heejun1105@users.noreply.github.com>
2024-09-24 22:56:05 +0900

    레포지터리 수정

--- src/main/java/com/example/demo/member/MemberRepository.java
+++ src/main/java/com/example/demo/member/MemberRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	
 	Optional<Member> findByMemberNum(Long memberNum);
+	Optional<Member> findByMemberId(String memberId);
 
 }
