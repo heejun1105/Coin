083fb0598b76a313ad2fc457ebb0614a22008a54
heejun1105 <163511743+heejun1105@users.noreply.github.com>
2024-09-25 18:59:37 +0900

    멤버서비스 수정

--- src/main/java/com/example/demo/member/MemberService.java
+++ src/main/java/com/example/demo/member/MemberService.java
@@ -10,10 +10,20 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 	
+	//멤버넘으로 회원찾기
 	public Member findMemberByMemberNum(Long MemberNum) {
 		
 		Member member = memberRepository.findByMemberNum(MemberNum).get();
 		
 		return member;
 	}
+	
+	//멤버ID로 회원찾기
+	public Member findMemberByMemberId(String MemberId) {
+		
+		Member member = memberRepository.findByMemberId(MemberId).get();
+		
+		return member;
+	}
+	
 }
