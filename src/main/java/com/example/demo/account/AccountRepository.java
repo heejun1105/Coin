2e2fc088ff5179f90fcc1914caf010315e6b705e
heejun1105 <163511743+heejun1105@users.noreply.github.com>
2024-10-12 00:21:12 +0900

    Update AccountRepository.java

--- src/main/java/com/example/demo/account/AccountRepository.java
+++ src/main/java/com/example/demo/account/AccountRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	
+	//AccountNum으로 Account찾기
 	Optional<Account> findByAccountNum(Long accountNum);
 
 }
