f91fff22f6c1794ef7ee7f89bb138d0d30b49137
heejun1105 <163511743+heejun1105@users.noreply.github.com>
2024-10-19 10:02:45 +0900

    Update AccountService.java

--- src/main/java/com/example/demo/account/AccountService.java
+++ src/main/java/com/example/demo/account/AccountService.java
@@ -2,7 +2,10 @@ package com.example.demo.account;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AccountService {
 
 }
